f29f3f534c0cd686628df6a7da34b9c51558cb1f
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-02 11:53:18 +0530

    added basic todo object

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+
+import { useState } from "react";
+
+interface todo {
+  id: string,
+  text: string,
+  completed: boolean,
+}
+
 export default function Home() {
+  const [todos, setTodos] = useState<todo[]>([
+    {
+      id: Math.random().toString(),
+      text: 'First Todo',
+      completed: false
+    },
+    {
+      id: Math.random().toString(),
+      text: 'Second Todo',
+      completed: true
+    },
+    {
+      id: Math.random().toString(),
+      text: 'Third Todo',
+      completed: false
+    }
+  ])
+
   return (
     <div>
-      Is this working?
+      {todos.map(todo => <div key={todo.id}>{todo.text}</div>)}
     </div>
   );
 }
